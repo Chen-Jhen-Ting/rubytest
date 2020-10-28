b7f585a7c38520e27efaed928cd75f3ff39dfc12
Ted Chen <teddy70642000@gmail.com>
2020-10-28 14:48:53 +0800

    coupon view change index & route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
-
+  get "/coupon", to: "coupons#index"
+  
   resources :users, only: [:create] do
     collection do
       get :sign_up
@@ -12,4 +13,11 @@ Rails.application.routes.draw do
       delete :sign_out
     end
   end
+
+  resources :coupons, only: [] do
+    collection do
+      get :change
+    end
+  end
+
 end
