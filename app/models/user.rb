ed5178093045a4cc350e7f2141696ca4e83beedd
Ted Chen <teddy70642000@gmail.com>
2020-10-28 14:33:29 +0800

    user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  before_create :encrypt_password
+
+  def self.login(options)
+    if options[:email] && options[:password]
+      find_by(email: options[:email],
+              password: Digest::SHA1.hexdigest("x" + options[:password] + "y"))
+    end
+  end
+
+  private
+  def encrypt_password
+    self.password = bigbang(self.password)
+  end
+
+  def bigbang(string)
+    string = "x" + string + "y"
+    Digest::SHA1.hexdigest(string)
+  end
 end
