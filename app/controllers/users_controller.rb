7c5ce7596a74cca0387eec8da179e3a1fc040498
Ted Chen <teddy70642000@gmail.com>
2020-10-28 14:39:03 +0800

    user create & session given

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # 登入
+      sign_in_user(@user)
+      redirect_to root_path, notice: '會員註冊成功'
+    else
+      render :sign_up
+    end
+  end
+
   private
+  def sign_in_user(u)
+    session[:user_token] = u.id
+  end
+
   def user_params
     params.require(:user).permit(:name, :password, :email)
   end
